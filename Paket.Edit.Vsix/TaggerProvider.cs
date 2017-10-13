537db566abf44c3691288ef90a55d08c1001b094
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-14 01:12:51 +0200

    Lock content type

--- Paket.Edit.Vsix/TaggerProvider.cs
+++ Paket.Edit.Vsix/TaggerProvider.cs
@@ -8,10 +8,20 @@
 
     [Export(typeof(ITaggerProvider))]
     [TagType(typeof(IOutliningRegionTag))]
-    [ContentType("LOCK")]
+    [ContentType("lock")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
     internal sealed class TaggerProvider : ITaggerProvider
     {
+        [Export]
+        [Name("lock")]
+        [BaseDefinition("text")]
+        internal static ContentTypeDefinition LockContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".lock")]
+        [ContentType("lock")]
+        internal static FileExtensionToContentTypeDefinition LockFileExtensionDefinition;
+
         public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
         {
             if (typeof(T) == typeof(IOutliningRegionTag))
